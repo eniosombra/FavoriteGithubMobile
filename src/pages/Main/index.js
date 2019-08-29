cac52bb0d53c6dde357278d570f3c61544fcb530
Enio L. Sombra <eniosombra@gmail.com>
2019-08-29 16:43:14 -0300

    feat: User and Repository

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,114 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {Keyboard, ActivityIndicator} from 'react-native';
 
-import {Container} from './styles';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
 
-export default function Main() {
-  return (
-    <Container>
-      <Text>Main Component</Text>
-    </Container>
-  );
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
+
+export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  state = {
+    newUser: '',
+    users: [],
+    loading: false,
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  handleAddUser = async () => {
+    const {users, newUser} = this.state;
+
+    this.setState({loading: true});
+
+    const response = await api.get(`/users/${newUser}`);
+
+    console.tron.log('ok');
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+      loading: false,
+    });
+
+    Keyboard.dismiss();
+  };
+
+  handleNavigate = user => {
+    const {navigation} = this.props;
+
+    navigation.navigate('User', {user});
+  };
+
+  render() {
+    const {users, newUser, loading} = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            returnKeyLabel="send"
+            onSubmitEditing={this.handleAddUser}
+            onChangeText={text => this.setState({newUser: text})}
+          />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({item}) => (
+            <User>
+              <Avatar source={{uri: item.avatar}} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
+      </Container>
+    );
+  }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
